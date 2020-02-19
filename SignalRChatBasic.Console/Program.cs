2524d602285868dbbcd795eee1d4a5db2639f3f8
Maddy Leger <maleger@microsoft.com>
2020-02-19 17:29:49 -0500

    Add project files.

---
+++ SignalRChatBasic.Console/Program.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace SignalRChatBasic.ConsoleApp
+{
+    class Program
+    {
+        static HubConnection connection;
+        static string name;
+
+        public static async Task Main(string[] args)
+        {
+            name = "maddy console";
+            Console.WriteLine("Enter IP:");
+            var ip = Console.ReadLine();
+            connection = new HubConnectionBuilder()
+                .WithUrl(ip + "/chatHub")
+                .Build();
+
+            await connection.StartAsync();
+            Console.WriteLine("You are connected!");
+
+            // Handle receiving new messages
+            connection.On<string, string>("ReceiveMessage", (user, message) =>
+            {
+                Console.WriteLine($"{user}: {message}");
+            });
+
+            var keepGoing = true;
+            do
+            {
+                var text = Console.ReadLine();
+                if (text == "exit")
+                {
+                    keepGoing = false;
+                }
+                else
+                {
+                    await connection.InvokeAsync("SendMessage", name, text);
+                }
+            }
+            while (keepGoing);
+        }
+    }
+}
---
+++ SignalRChatBasic/SignalRChatBasic/Views/ChatPage.xaml.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SignalRChatBasic.Views
+{
+
+    public partial class ChatPage : ContentPage
+    {
+        HubConnection connection;
+        ObservableCollection<string> messages = new ObservableCollection<string>();
+        public ChatPage()
+        {
+            InitializeComponent();
+
+            // Connects to backend - "Hub" class route
+            connection = new HubConnectionBuilder()
+                .WithUrl(serverEntry.Text + "/chatHub")
+                .Build();
+
+            messagesList.ItemsSource = messages;
+        }
+
+        private async void ConnectButton_Clicked(object sender, EventArgs e)
+        {
+            // Handle receiving new messages
+            connection.On<string, string>("ReceiveMessage", (user, message) =>
+            {
+                this.Dispatcher.BeginInvokeOnMainThread(() =>
+                {
+                    var newMessage = $"{user}: {message}";
+                    messages.Add(newMessage);
+                });
+            });
+
+            // try to connect!
+            try
+            {
+                await connection.StartAsync();
+                messages.Add("Starting connection!!!!");
+            }
+            catch (Exception ex)
+            {
+                messages.Add(ex.Message);
+            }
+        }
+
+        private async void SendButton_Clicked(object sender, EventArgs e)
+        {
+            // Use the "SendMessage" hub method
+            try
+            {
+                await connection.InvokeAsync("SendMessage",
+                    nameEntry.Text, messageEntry.Text);
+            }
+            catch (Exception ex)
+            {
+                messages.Add(ex.Message);
+            }
+        }
+    }
+}
